0cd61219cafd9d424fca17ca1aba20426c8f0f45
EndarMuh <2141720160@student.polinema.ac.id>
2023-09-05 15:52:54 +0700

    Week 2

---
+++ Pertemuan 2/src/no1.dart
@@ -0,0 +1,13 @@
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print('Nama saya adalah Muhammad Endar Darmawan, sekarang berumur ${18 - i}');
+  }
+}
+
+
+// atau jika dirubah logikanya
+// void main() {
+//   for (int i = 16; i >= 7; i--) {
+//     print(18 - i);
+//   }
+// }
