1caaefc1bdaf5f3bcfd0b17b0dd9b9afa2fa6020
EndarMuh <2141720160@student.polinema.ac.id>
2023-10-09 02:18:37 +0700

    Revisi JS 5

--- Pertemuan 5/docs/hello_world/lib/main.dart
+++ Pertemuan 5/docs/hello_world/lib/main.dart
@@ -22,23 +22,43 @@
 //   }
 // }
 
-import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widget/fab_widget.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hello_world/basic_widget/fab_widget.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  // this widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Row(
-        children: [Expanded(child: MyFabWidget())],
-      ),
-    );
-  }
-}
+//   // this widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Row(
+//         children: [Expanded(child: MyFabWidget())],
+//       ),
+//     );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+// // import 'basic_widget/text_widget.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Text(
+//         "Nama saya Muhammad Endar Darmawan, sedang belajar Pemrograman Mobile",
+//         style: TextStyle(color: Colors.red, fontSize: 14),
+//         textAlign: TextAlign.center);
+//   }
+// }
+
