eff5f260b1bd70bb4754371e8ad4ead3859341bd
EndarMuh <2141720160@student.polinema.ac.id>
2023-10-15 22:08:59 +0700

    Tambah JS 7

--- Pertemuan 7/layout_flutter/lib/main.dart
+++ Pertemuan 7/layout_flutter/lib/main.dart
@@ -289,6 +289,116 @@
 // }
 
 // Praktikum 3
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget titleSection = Container(
+//       padding: const EdgeInsets.all(32.0),
+//       child: Row(
+//         children: [
+//           Expanded(
+//             /*Soal 1*/
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 /*Soal 2*/
+//                 Container(
+//                   padding: const EdgeInsets.only(bottom: 8.0),
+//                   child: const Text(
+//                     'Batu',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 Text(
+//                   'Malang, Indonesia',
+//                   style: TextStyle(
+//                     color: Colors.grey[500],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           /*Soal 3*/
+//           Icon(
+//             Icons.star,
+//             color: Colors.red[500],
+//           ),
+//           const Text('41'),
+//         ],
+//       ),
+//     );
+
+//     Column _buildButtonColumn(Color color, IconData icon, String label) {
+//       return Column(
+//         mainAxisSize: MainAxisSize.min,
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Icon(icon, color: color),
+//           Container(
+//             margin: const EdgeInsets.only(top: 8),
+//             child: Text(
+//               label,
+//               style: TextStyle(
+//                 fontSize: 12,
+//                 fontWeight: FontWeight.w400,
+//                 color: color,
+//               ),
+//             ),
+//           ),
+//         ],
+//       );
+//     }
+
+//     Color color = Theme.of(context).primaryColor;
+
+//     Widget buttonSection = Padding(
+//       padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           _buildButtonColumn(color, Icons.call, 'CALL'),
+//           _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+//           _buildButtonColumn(color, Icons.share, 'SHARE'),
+//         ],
+//       ),
+//     );
+
+//     Widget textSection = Container(
+//       padding: const EdgeInsets.all(32),
+//       child: const Text(
+//         'Batu adalah sebuah kota wisata di Provinsi Jawa Timur, Indonesia. Kota ini terletak 90 km sebelah barat daya Surabaya atau 15 km sebelah barat laut Malang. Kota Batu berada di jalur yang menghubungkan Malang-Kediri dan Jombang. '
+//         'Muhammad Endar Darmawan | 2141720160 ',
+//         softWrap: true,
+//       ),
+//     );
+
+//     return MaterialApp(
+//       title: 'Flutter layout: Muhammad Endar Darmawan | 2141720160',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Flutter layout demo'),
+//         ),
+//         body: Column(
+//           children: [
+//             titleSection,
+//             buttonSection,
+//             textSection,
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// Praktikum 4
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -381,13 +491,19 @@ class MyApp extends StatelessWidget {
     );
 
     return MaterialApp(
-      title: 'Flutter layout: Muhammad Endar Darmawan | 2141720160',
+      title: 'MyApp',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'img/view.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
